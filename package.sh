565e3bc381935b6041c46a91e399fbd8f1f6a9cc
Felipe Vilarinho <fvilarinho@innovativethinking.com.br>
2021-03-30 23:35:43 -0300

    Testing.

--- package.sh
+++ package.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker login -u fvilarinho -p $GITHUB_TOKEN docker.pkg.github.com
 docker build -t ghcr.io/fvilarinho/demo:latest .
--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+docker login -u fvilarinho -p $GITHUB_TOKEN docker.pkg.github.com
 docker push ghcr.io/fvilarinho/demo:latest

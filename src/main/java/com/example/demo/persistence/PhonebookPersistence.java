442cf97bcff4fc8ce833d0640b55b56a075647f8
Felipe Vilarinho <fvilarinho@innovativethinking.com.br>
2021-03-31 19:49:57 -0300

    Update documentation.

--- src/main/java/com/example/demo/persistence/PhonebookPersistence.java
+++ src/main/java/com/example/demo/persistence/PhonebookPersistence.java
@@ -2,15 +2,11 @@ package com.example.demo.persistence;
 
 import com.example.demo.model.Phonebook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-/**
- * Interface that defines the persistence of the data model.
- *
- * @author fvilarinho
- */
 @Repository
 public interface PhonebookPersistence extends JpaRepository<Phonebook, Integer>{
     public List<Phonebook> findByNameContaining(String name);

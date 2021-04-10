d1d159e7e337b865dbae7ce9440aded0a1def7da
Felipe Vilarinho <fvilarinho@innovativethinking.com.br>
2021-04-10 16:08:41 -0300

    Testing.

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Build the docker image.
+
 mkdir etc
 echo "$CONTRAST_CONFIG_DATA" > etc/contrast_security.yaml
-docker-compose build
+docker build -t $DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_USERNAME/demo:latest
 rm -rf etc
---
+++ packageAnalysis.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+snyk container monitor $DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_USERNAME/demo:latest

4651642be56a7c344a7b99d8c1538282264d0022
Felipe Vilarinho <fvilarinho@innovativethinking.com.br>
2021-09-10 18:44:12 -0300

    Update pipeline.

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,13 @@ fi
 if [ ! -z "$KUBECONFIG_DATA" ]; then
   mkdir -p ~/.kube#
 
-  echo "$KUBECONFIG_DATA" > ~/.kube/config.tmp
+  echo "$KUBECONFIG_DATA" > ~/.kubeconfig
 
-  base64 -d -i ~/.kube/config.tmp > ~/.kube/config
+  KUBECONFIG_DATA=`base64 -d -i ~/.kubeconfig`
 
-  rm ~/.kube/config.tmp
+  echo "$KUBECONFIG_DATA" > ~/.kube/config
+
+  rm ~/.kubeconfig
 fi
 
 $KUBECTL_CMD set image statefulset database database=$DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_USER/demo-database -n demo

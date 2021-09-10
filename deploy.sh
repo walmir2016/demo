640f3ccb7aa5695f9837f573cb1db1ac5901c75c
Felipe Vilarinho <fvilarinho@innovativethinking.com.br>
2021-09-10 18:24:56 -0300

    Update pipeline.

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,18 @@ if [ -z "$KUBECTL_CMD" ]; then
   KUBECTL_CMD=./kubectl
 fi
 
-#if [ ! -z "$KUBECONFIG" ]; then
-#  mkdir -p ~/.kube#
+if [ ! -z "$KUBECONFIG" ]; then
+  mkdir -p ~/.kube#
 
-#  echo "$KUBECONFIG" > ~/.kube/config
+  echo "$KUBECONFIG" > ~/.kube/config
 
-#  KUBECONFIG_DATA=`base64 -d -i ~/.kube/config`
+  KUBECONFIG=`base64 -d -i ~/.kube/config`
 
-#  echo "$KUBECONFIG_DATA" > ~/.kube/config
-#fi
+  echo "$KUBECONFIG" > ~/.kube/config
+
+  KUBECONFIG=~/.kube/config
+fi
 
 $KUBECTL_CMD set image statefulset database database=$DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_USER/demo-database -n demo
-#$KUBECTL_CMD set image deployment backend backend=$DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_USER/demo-backend -n demo
-#$KUBECTL_CMD set image deployment frontend frontend=$DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_USER/demo-frontend -n demo
+$KUBECTL_CMD set image deployment backend backend=$DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_USER/demo-backend -n demo
+$KUBECTL_CMD set image deployment frontend frontend=$DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_USER/demo-frontend -n demo

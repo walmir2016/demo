f511f5e55d6b13ddf4aa79b8b024730661b4c695
Felipe <fvilarinho@gmail.com>
2021-09-23 13:08:37 -0300

    Adding RASP.

--- backend/bin/startup.sh
+++ backend/bin/startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Set debug and RASP environment variables.
 export JPDA_OPTS="$JPDA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:8000"
 export CATALINA_OPTS="$CATALINA_OPTS -javaagent:/home/user/lib/contrast.jar"
 
+# Startup script in debug mode.
 /opt/apache-tomcat/bin/catalina.sh jpda run

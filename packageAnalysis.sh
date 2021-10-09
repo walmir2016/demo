0df1a111b79ec1d2a6ab5f6ed3e1c3995d013cdb
Felipe <fvilarinho@gmail.com>
2021-10-09 17:40:19 -0300

    Updating SAST scripts.

--- packageAnalysis.sh
+++ packageAnalysis.sh
@@ -6,7 +6,9 @@ if [ -z "$SNYK_CMD" ]; then
   SNYK_CMD=./snyk
 fi
 
-docker save ghcr.io/fvilarinho/demo-database:latest -o demo-database.tar
+BUILD_VERSION=`sed 's/BUILD_VERSION=//g' .env`
+
+docker save ghcr.io/fvilarinho/demo-database:$BUILD_VERSION -o demo-database.tar
 
 $SNYK_CMD container test --severity-threshold=high docker-archive:demo-database.tar --file=./database/Dockerfile
 
@@ -15,7 +17,7 @@ status=`echo $?`
 rm -f demo-database.tar
 
 if [ $status -eq 0 ]; then
-	docker save ghcr.io/fvilarinho/demo-backend:latest -o demo-backend.tar
+	docker save ghcr.io/fvilarinho/demo-backend:$BUILD_VERSION -o demo-backend.tar
 
 	$SNYK_CMD container test --severity-threshold=high docker-archive:demo-backend.tar --file=./backend/Dockerfile
 
@@ -24,7 +26,7 @@ if [ $status -eq 0 ]; then
 	rm -f demo-backend.tar
 
 	if [ $status -eq 0 ]; then
-		docker save ghcr.io/fvilarinho/demo-frontend:latest -o demo-frontend.tar
+		docker save ghcr.io/fvilarinho/demo-frontend:$BUILD_VERSION -o demo-frontend.tar
 
 		$SNYK_CMD container test --severity-threshold=high docker-archive:demo-frontend.tar --file=./frontend/Dockerfile
 

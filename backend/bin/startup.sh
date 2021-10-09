f6f5ea1c6e1686c9f09b8c87eb4b0fd2d5687a2f
Felipe <fvilarinho@gmail.com>
2021-10-09 15:31:25 -0300

    Adding RASP.

--- backend/bin/startup.sh
+++ backend/bin/startup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Check if the database is up and running.
+while [ true ]; do
+  nc -z database 3306
+
+  if [ $? -eq 0 ]; then
+    break
+  else
+    sleep 1
+  fi
+done
+
 # Set debug and RASP environment variables.
 export JPDA_OPTS="$JPDA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:8000"
 export CATALINA_OPTS="$CATALINA_OPTS -javaagent:/home/user/lib/contrast.jar"
--- deploy.sh
+++ deploy.sh
@@ -11,13 +11,17 @@ if [ ! -z "$KUBECONFIG_TOKEN" ]; then
 
   echo "$KUBECONFIG_TOKEN" > ~/.kubeconfig
 
-  $KUBECONFIG_TOKEN=`base64 -d -i ~/.kubeconfig`
+  KUBECONFIG_TOKEN=`base64 -d -i ~/.kubeconfig`
 
   echo "$KUBECONFIG_TOKEN" > ~/.kube/config
 
   rm ~/.kubeconfig
 fi
 
-$KUBECTL_CMD set image statefulset database database=ghcr.io/fvilarinho/demo-database -n demo
-$KUBECTL_CMD set image daemonset backend backend=ghcr.io/fvilarinho/demo-backend -n demo
-$KUBECTL_CMD set image daemonSet frontend frontend=ghcr.io/fvilarinho/demo-frontend -n demo
+BUILD_VERSION=`sed 's/BUILD_VERSION=//g' .env`
+
+rm .env
+
+$KUBECTL_CMD set image statefulset database database=ghcr.io/fvilarinho/demo-database:$BUILD_VERSION -n demo
+$KUBECTL_CMD set image daemonset backend backend=ghcr.io/fvilarinho/demo-backend:$BUILD_VERSION -n demo
+$KUBECTL_CMD set image daemonSet frontend frontend=ghcr.io/fvilarinho/demo-frontend:$BUILD_VERSION -n demo

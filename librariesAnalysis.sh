6eff8503bec203cbef6d3ee0ab7c192c69feff26
fvilarinho <fvilarinho@gmail.com>
2022-03-10 13:42:45 -0300

    Testing

--- librariesAnalysis.sh
+++ librariesAnalysis.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-SNYK_CMD=`which snyk`
-
-if [ -z "$SNYK_CMD" ]; then
-  SNYK_CMD=./snyk
-fi
-
-$SNYK_CMD test backend --severity-threshold=high
+snyk test ./backend --severity-threshold=high

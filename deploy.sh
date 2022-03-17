bc7b1087a18b21fa526be34e3677232489913ce2
fvilarinho <fvilarinho@gmail.com>
2022-03-17 00:32:54 -0300

    Change continuous deployment workflow.

--- deploy.sh
+++ deploy.sh
@@ -2,64 +2,60 @@
 
 cd iac
 
-export BUILD_VERSION=`sed 's/BUILD_VERSION=//g' .env`
-export PROVISIONED=`dig +short cluster-manager.$CLOUDFLARE_ZONE_NAME`
-export KUBECTL_CMD=`which kubectl`
-
 echo "$DIGITALOCEAN_PRIVATE_KEY" > /tmp/.id_rsa
 
 if [ -f "/tmp/.id_rsa" ]; then
   chmod og-rwx /tmp/.id_rsa
 fi
 
+export KUBECTL_CMD=`which kubectl`
+
 if [ -z "$KUBECTL_CMD" ]; then
   KUBECTL_CMD=./kubectl
 fi
 
-if [ -z "$PROVISIONED" ]; then
-  TERRAFORM_CMD=`which terraform`
+export TERRAFORM_CMD=`which terraform`
 
-  if [ -z "$TERRAFORM_CMD" ]; then
-    TERRAFORM_CMD=./terraform
-  fi
-
-  $TERRAFORM_CMD init
-  $TERRAFORM_CMD apply -auto-approve \
-                       -var "digitalocean_token=$DIGITALOCEAN_TOKEN" \
-                       -var "digitalocean_public_key=$DIGITALOCEAN_PUBLIC_KEY" \
-                       -var "digitalocean_private_key=$DIGITALOCEAN_PRIVATE_KEY" \
-                       -var "cloudflare_email=$CLOUDFLARE_EMAIL" \
-                       -var "cloudflare_api_key=$CLOUDFLARE_API_KEY" \
-                       -var "cloudflare_zone_id=$CLOUDFLARE_ZONE_ID" \
-                       -var "cloudflare_zone_name=$CLOUDFLARE_ZONE_NAME" \
-                       -var "datadog_agent_key=$DATADOG_AGENT_KEY"
-
-  CLUSTER_MANAGER_IP=`cat cluster-manager-ip`
-
-  rm cluster-manager-ip
-
-  scp -i /tmp/.id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$CLUSTER_MANAGER_IP:/etc/rancher/k3s/k3s.yaml ./.kubeconfig
-
-  sed -i -e 's|127.0.0.1|'"$CLUSTER_MANAGER_IP"'|g' ./.kubeconfig
-
-  cp ./kubernetes.yml /tmp/kubernetes.yml
-
-  sed -i -e 's|${BUILD_VERSION}|'"$BUILD_VERSION"'|g' /tmp/kubernetes.yml
-
-  $KUBECTL_CMD --kubeconfig=./.kubeconfig apply -f /tmp/kubernetes.yml
-
-  rm -d /tmp/kubernetes.yml
-else
-  scp -i /tmp/.id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@cluster-manager.$CLOUDFLARE_ZONE_NAME:/etc/rancher/k3s/k3s.yaml ./.kubeconfig
-
-  sed -i -e 's|127.0.0.1|'"cluster-manager.$CLOUDFLARE_ZONE_NAME"'|g' ./.kubeconfig
-
-  $KUBECTL_CMD --kubeconfig=./.kubeconfig set image deployment database database=felipevilarinho/demo-database:$BUILD_VERSION
-  $KUBECTL_CMD --kubeconfig=./.kubeconfig set image daemonset backend backend=felipevilarinho/demo-backend:$BUILD_VERSION
-  $KUBECTL_CMD --kubeconfig=./.kubeconfig set image daemonSet frontend frontend=felipevilarinho/demo-frontend:$BUILD_VERSION
+if [ -z "$TERRAFORM_CMD" ]; then
+  TERRAFORM_CMD=./terraform
 fi
 
+if [ ! -d "~/.terraform.d" ]; then
+  mkdir -p ~/.terraform.d
+fi
+
+if [ ! -f "~/.terraform.d/credentials.tfrc.json" ]; then
+  cp credentials.tfrc.json /tmp
+
+  sed -i -e 's|${TERRAFORM_TOKEN}|'"$TERRAFORM_TOKEN"'|g' /tmp/credentials.tfrc.json
+
+  mv /tmp/credentials.tfrc.json ~/.terraform.d
+fi
+
+$TERRAFORM_CMD init
+$TERRAFORM_CMD apply -auto-approve \
+                     -var "digitalocean_token=$DIGITALOCEAN_TOKEN" \
+                     -var "digitalocean_public_key=$DIGITALOCEAN_PUBLIC_KEY" \
+                     -var "digitalocean_private_key=$DIGITALOCEAN_PRIVATE_KEY" \
+                     -var "datadog_agent_key=$DATADOG_AGENT_KEY"
+
+export CLUSTER_MANAGER_IP=$($TERRAFORM_CMD output -raw cluster-manager-ip)
+
+scp -i /tmp/.id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$CLUSTER_MANAGER_IP:/etc/rancher/k3s/k3s.yaml ./.kubeconfig
+
+sed -i -e 's|127.0.0.1|'"$CLUSTER_MANAGER_IP"'|g' ./.kubeconfig
+
+cp ./kubernetes.yml /tmp/kubernetes.yml
+
+export BUILD_VERSION=`sed 's/BUILD_VERSION=//g' .env`
+
+sed -i -e 's|${BUILD_VERSION}|'"$BUILD_VERSION"'|g' /tmp/kubernetes.yml
+
+$KUBECTL_CMD --kubeconfig=./.kubeconfig apply -f /tmp/kubernetes.yml
+
+rm -f /tmp/kubernetes.yml
 rm -f ./.kubeconfig*
 rm -f /tmp/.id_rsa
+rm -f ./cluster-manager-ip
 
 cd ..
--- iac/variables.tf
+++ iac/variables.tf
@@ -2,17 +2,4 @@ variable "digitalocean_token" {}
 variable "digitalocean_public_key" {}
 variable "digitalocean_private_key" {}
 
-variable "cloudflare_email" {}
-variable "cloudflare_api_key" {}
-variable "cloudflare_zone_id" {}
-variable "cloudflare_zone_name" {}
-
 variable "datadog_agent_key" {}
-
-#resource "random_string" "password" {
-#  length = 32
-#  special = true
-#  upper = true
-#  lower = true
-#  number = true
-#}
